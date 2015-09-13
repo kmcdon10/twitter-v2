d8316316aefefbcdf9d6674019f617c8f55973fd
Kate <katemcdonald39@gmail.com>
2015-09-13 18:25:29 -0400

    finished workshop -gained a better understanding of express

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 var express = require("express");
 var app = express();
+var swig = require("swig");
+var routes = require("./routes");
+var router = routes(io);
+var bodyParser = require("body-parser");
+var socketio = require("socket.io");
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "html");
+app.engine("html", swig.renderFile);
+swig.setDefaults({cache: false});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use("/", router);
+//app.use("/users", users);
+app.use(express.static("public"));
 
 
-app.get("/", function(req, res, next) {
-	res.send("hello kate");
+var port = 3000;
+var server = app.listen(port, function() {
+	console.log("server listening on port ", port);
 });
-
-
-var server = app.listen(300, function() {
-	console.log("server listening on port ", server.address().port);
-});
+var io = socketio.listen(server);
